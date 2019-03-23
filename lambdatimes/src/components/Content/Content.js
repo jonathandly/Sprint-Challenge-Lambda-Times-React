65383efd8f36cdb542b2e57c96c282dea5b50762
Jonathan Daly <dalyj138@gmail.com>
2019-03-23 13:02:23 -0400

    Cards display completed.

--- lambdatimes/src/components/Content/Content.js
+++ lambdatimes/src/components/Content/Content.js
@@ -23,6 +23,9 @@ export default class Content extends Component {
 
   changeSelected = tab => {
     // this function should take in the tab and update the state with the new tab.
+    // this.setState({ tabs: this.state.tabs.push(tab) });
+    // tab = this.state.selected;
+    // this.setState({ tabs: tab });
   };
 
   filterCards = () => {
@@ -49,7 +52,11 @@ export default class Content extends Component {
           `selectedTab` that includes the currently selected tab
           and `selectTabHandler` that includes the function to change the selected tab
         */}
-        <Tabs tabs={this.state.tabs} />
+        <Tabs 
+          tabs={this.state.tabs} 
+          selectedTab={this.state.selected} 
+          selectTabHandler={this.changeSelected}
+        />
         <Cards cards={this.filterCards()} />
       </div>
     );
